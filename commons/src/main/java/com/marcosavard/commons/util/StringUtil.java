6385ab6a029c4eafeee92f40465bdc121967eab0
Marco Savard <savard.marco@gmail.com>
2018-09-27 20:10:04 -0400

    added methods in StringUtil

--- commons/src/main/java/com/marcosavard/commons/util/StringUtil.java
+++ commons/src/main/java/com/marcosavard/commons/util/StringUtil.java
@@ -5,39 +5,46 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * A utility class that provides useful methods that handle strings. 
+ * A utility class that provides useful methods that handle strings. It includes
+ * methods to predicate whether a string is null or empty, to pad strings with
+ * blanks, to remove quotes from strings, to strip accents from a string.
  * 
  * @author Marco
  *
  */
 public class StringUtil {
-	private static final Character[] VOYELS = new Character[] {'a', 'e', 'i', 'o', 'u'};
-	
+	private static final String ELLIPSIS = "\u2026"; // ... character
+	private static final Character[] VOYELS = new Character[] { 'a', 'e', 'i', 'o', 'u' };
+
+	// Utility class is not instantiable
+	private StringUtil() {
+	}
+
 	/**
-	 * Capitalize the original string.  
+	 * Capitalize the original string.
 	 * 
 	 * @param original
 	 * @return
 	 */
 	public static String capitalize(String original) {
-		String s = (original == null) ? "" : original; 
-		s = (s.length() == 0) ? s : s.substring(0, 1).toUpperCase() + s.substring(1); 
-		return s; 
+		String s = (original == null) ? "" : original;
+		s = (s.length() == 0) ? s : s.substring(0, 1).toUpperCase() + s.substring(1);
+		return s;
 	}
-	
+
 	/**
-	 * Tells if text is null or empty 
+	 * Returns the same string, but empty is null.
 	 * 
-	 * @param text
-	 * @return true if null or empty
+	 * @param text to check
+	 * @return text or "" is text is empty
 	 */
-	public static boolean isNullOrEmpty(String text) {
-		boolean nullOrEmpty = (text == null) || text.isEmpty();
-		return nullOrEmpty;
+	public static String emptyIfNull(String text) {
+		String dest = (text == null) ? "" : text;
+		return dest;
 	}
-	
+
 	/**
-	 * Tells if text is null or blank 
+	 * Tells if text is null or blank
 	 * 
 	 * @param text
 	 * @return true if null or blank
@@ -48,45 +55,125 @@ public class StringUtil {
 	}
 
 	/**
+	 * Tells if text is null or empty
+	 * 
+	 * @param text
+	 * @return true if null or empty
+	 */
+	public static boolean isNullOrEmpty(String text) {
+		boolean nullOrEmpty = (text == null) || text.isEmpty();
+		return nullOrEmpty;
+	}
+
+	/**
 	 * Tells if c is a voyel
+	 * 
 	 * @param c
 	 * @return true if voyel
 	 */
 	public static boolean isVoyel(char c) {
-		c = Character.toLowerCase(c); 
-		List<Character> voyels = Arrays.asList(VOYELS); 
+		c = Character.toLowerCase(c);
+		List<Character> voyels = Arrays.asList(VOYELS);
 		boolean voyel = voyels.contains(c);
 		return voyel;
 	}
 	
 	/**
+	 * Pad n blanks at left 
+	 * 
+	 * @param source
+	 * @param n of blanks
+	 * @return
+	 */
+    public static String padLeftBlanks(String source, int n) {
+        String padded = (n > 0) ? String.format("%1$" + n + "s", source) : source;
+        return padded;
+    }
+
+	/**
 	 * Strip off accents from characters
+	 * 
 	 * @param text with accents
 	 * @return stripped text
 	 */
 	public static String stripAccents(String text) {
 		String stripped = Normalizer.normalize(text, Normalizer.Form.NFD);
 		stripped = stripped.replaceAll("[^\\p{ASCII}]", "");
-		return stripped; 
+		return stripped;
+	}
+
+	/**
+	 * Return a string in which all the blanks (whitespaces and tabs) of the text
+	 * parameter are stripped off. For instance, stripBlanks("hello world") returns
+	 * "helloworld".
+	 * 
+	 * @param text the original String, that may contain characters with blanks.
+	 * @return the same String, but without blanks
+	 */
+	public static String stripBlanks(String text) {
+		// remove whitespaces and tabs
+		text = text.replaceAll("\\s", "");
+		return text;
 	}
 
 	/**
 	 * Strip off non digit characters
+	 * 
 	 * @param text
 	 * @return
 	 */
 	public static String stripNonDigit(String text) {
-		StringBuilder sb = new StringBuilder(); 
+		StringBuilder sb = new StringBuilder();
 		int nb = text.length();
-		
-		for (int i=0; i<nb; i++) {
-			char ch = text.charAt(i); 
+
+		for (int i = 0; i < nb; i++) {
+			char ch = text.charAt(i);
 			if (Character.isDigit(ch)) {
-				sb.append(ch); 
+				sb.append(ch);
 			}
 		}
-		
+
 		return sb.toString();
 	}
+
+	/**
+	 * Truncate source at a given length
+	 * 
+	 * @param source
+	 * @param lenght to be truncate
+	 * @return a string having at most 'lenght' characters
+	 */
+	public static String truncate(String source, int lenght) {
+		String truncated = (source == null) ? "" : source;
+		boolean trunk = truncated.length() > lenght;
+		truncated = trunk ? truncated.substring(0, lenght - 1) + ELLIPSIS : truncated;
+		return truncated;
+	}
+	
+	/**
+     * Returns the same String, but without the quotes. For instance,
+     * removeSurroundingQuotes("'text'", '\'') returns "text".
+     * 
+     * @param text whose the first and last characters may be quote characters.
+     * 
+     * @return the text without the quotes, if any
+     */
+    public static String unquote(String text, char quoteCharacter) {
+        String target = "";
+
+        if (!isNullOrBlank(text)) {
+            char ch = text.charAt(0);
+            target = (ch == quoteCharacter) ? text.substring(1) : text;
+
+            if (!isNullOrBlank(target)) {
+                int len = target.length() - 1;
+                ch = target.charAt(len);
+                target = (ch == quoteCharacter) ? target.substring(0, len) : target;
+            }
+        }
+
+        return target;
+    }
+
 
 }
