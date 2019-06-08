b84a90c4ef5ea4bbb00d5ab8de011705ed174ca1
Marco Savard <savard.marco@gmail.com>
2019-06-08 09:54:24 -0400

    Improved password

--- commons/src/main/java/com/marcosavard/commons/security/PasswordDemo.java
+++ commons/src/main/java/com/marcosavard/commons/security/PasswordDemo.java
@@ -1,32 +1,28 @@
 package com.marcosavard.commons.security;
 
-import java.text.MessageFormat;
-
 import com.marcosavard.commons.security.Password.Strenght;
+import com.marcosavard.commons.util.ToStringBuilder;
 
 public class PasswordDemo {
 
-	//Store passwords in String just for demo or testing purposes
-	//In production code, always store passwords in char[], and clear the password
-	//once the hash is computed
-	
-	public static void main(String[] args) {
+  // Store passwords in String just for demo or testing purposes
+  // In production code, always store passwords in char[], and clear the password
+  // once the hash is computed
 
-		String[] passwords = new String[] {
-			"abc", 
-			"password",
-			"P@ssw0rd",
-			"horse equitation stable zebra" 
-		};
-		
-		for (String pw : passwords) {
-			int enthropy = Password.computeEntropy(pw.toCharArray()); 
-			Strenght strength = Password.getStrenght(enthropy);
-			String msg = MessageFormat.format("  ''{0}'' enthropy = {1} bits ({2})", pw, enthropy, strength);
-			System.out.println(msg);
-		}
-		
-		
-	}
+  public static void main(String[] args) {
+
+    String[] entries =
+        new String[] {"abc", "abc   ", "password", "P@ssw0rd", "horse equitation stable zebra"};
+
+    for (String entry : entries) {
+      Password password = Password.of(entry.toCharArray());
+      int enthropy = password.getEntropy();
+      Strenght strength = password.getStrenght();
+      System.out.println(ToStringBuilder.build(password));
+      password.clear();
+    }
+
+
+  }
 
 }
