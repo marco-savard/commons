b6c58c13dd7b0c9d7b088aed9f2064cf49ec600f
Marco Savard <savard.marco@gmail.com>
2019-06-11 05:49:46 -0400

    Changes to Color

--- commons/src/main/java/com/marcosavard/commons/types/ColorDemo.java
+++ commons/src/main/java/com/marcosavard/commons/types/ColorDemo.java
@@ -2,19 +2,19 @@ package com.marcosavard.commons.types;
 
 public class ColorDemo {
 
-	public static void main(String[] args) {
-		Color primary = new Color(255, 0, 0); 
-		
-		int color = 0x00ff00; 
-		int green = color & 0x00ff00; 
-		green = green / 256;
-		
-		System.out.println(" green : " + green);
-		
-		//blend with
-		
-		//constrast
+  public static void main(String[] args) {
+    Color primary = Color.of(255, 0, 0);
 
-	}
+    int color = 0x00ff00;
+    int green = color & 0x00ff00;
+    green = green / 256;
+
+    System.out.println(" green : " + green);
+
+    // blend with
+
+    // constrast
+
+  }
 
 }
--- commons/src/main/java/com/marcosavard/commons/types/ColorPalette.java
+++ commons/src/main/java/com/marcosavard/commons/types/ColorPalette.java
@@ -12,89 +12,89 @@ import java.util.Map;
 import com.marcosavard.commons.io.csv.CsvReader;
 
 /**
- * A color palette is simply a set of colors identified by a name. 
+ * A color palette is simply a set of colors identified by a name.
  * 
  * @author Marco
  *
  */
 public class ColorPalette {
-	//named colors: //http://html-color-codes.info/color-names/	
-	//named colors: //http://html-color-codes.info/color-names/
-	private static ColorPalette singleInstance; 
+  // named colors: //http://html-color-codes.info/color-names/
+  // named colors: //http://html-color-codes.info/color-names/
+  private static ColorPalette singleInstance;
 
-	public static ColorPalette getColorPalette() {
-		if (singleInstance == null) {
-			singleInstance = new ColorPalette(); 
-		}
-		
-		return singleInstance; 
-	}
+  public static ColorPalette getColorPalette() {
+    if (singleInstance == null) {
+      singleInstance = new ColorPalette();
+    }
 
-	private Map<String, Color> colorMap = new HashMap<>(); 
-	private List<NamedColor> namedColors = new ArrayList<>();
-	
-	public List<NamedColor> getNamedColors() {
-		return namedColors;
-	}
-	
-	private ColorPalette() {
-		try {
-			//read csv
-			InputStream input = ColorPalette.class.getResourceAsStream("ColorPalette.csv"); 
-			Reader reader = new InputStreamReader(input);
-			this.namedColors = readColors(reader);
-		} catch (IOException e) {
-			//File is missing
-			e.printStackTrace();
-		}
-	}
-	
-	private List<NamedColor> readColors(Reader reader) throws IOException {
-		CsvReader cr = new CsvReader(reader, 1, ';'); 
-		cr.readHeaderColumns(); 
-		List<NamedColor> namedColors = new ArrayList<>();  
-		
-		while (cr.hasNext()) {
-			List<String> line = cr.readNext();
-			
-			if (! line.isEmpty()) {
-				NamedColor namedColor = readColor(line); 
-				namedColors.add(namedColor); 
-			}
-		}
-		
-		return namedColors; 
-	}
-	
-	private NamedColor readColor(List<String> line) {
-		String code = line.get(0); 
-		String name = line.get(1); 
-		Color color = new Color(Integer.decode(code)); 
-		NamedColor namedColor = new NamedColor(color, name); 
-		return namedColor;
-	}
-	
-	public void addNamedColor(String colorName, int colorCode) {
-		colorMap.put(colorName, new Color(colorCode)); 
-	}
-	
-	public static class NamedColor {
-		private Color color; 
-		private String name; 
-		
-		public NamedColor(Color color, String name) {
-			this.color = color; 
-			this.name = name; 
-		} 
-		
-		public Color getColor() {
-			return color;
-		}
-		
-		@Override
-		public String toString() {
-			String msg = MessageFormat.format("{0} {1}", name, color.toRGBString()); 
-			return msg;
-		}	
-	}
+    return singleInstance;
+  }
+
+  private Map<String, Color> colorMap = new HashMap<>();
+  private List<NamedColor> namedColors = new ArrayList<>();
+
+  public List<NamedColor> getNamedColors() {
+    return namedColors;
+  }
+
+  private ColorPalette() {
+    try {
+      // read csv
+      InputStream input = ColorPalette.class.getResourceAsStream("ColorPalette.csv");
+      Reader reader = new InputStreamReader(input);
+      this.namedColors = readColors(reader);
+    } catch (IOException e) {
+      // File is missing
+      e.printStackTrace();
+    }
+  }
+
+  private List<NamedColor> readColors(Reader reader) throws IOException {
+    CsvReader cr = new CsvReader(reader, 1, ';');
+    cr.readHeaderColumns();
+    List<NamedColor> namedColors = new ArrayList<>();
+
+    while (cr.hasNext()) {
+      List<String> line = cr.readNext();
+
+      if (!line.isEmpty()) {
+        NamedColor namedColor = readColor(line);
+        namedColors.add(namedColor);
+      }
+    }
+
+    return namedColors;
+  }
+
+  private NamedColor readColor(List<String> line) {
+    String code = line.get(0);
+    String name = line.get(1);
+    Color color = Color.of(Integer.decode(code));
+    NamedColor namedColor = new NamedColor(color, name);
+    return namedColor;
+  }
+
+  public void addNamedColor(String colorName, int colorCode) {
+    colorMap.put(colorName, Color.of(colorCode));
+  }
+
+  public static class NamedColor {
+    private Color color;
+    private String name;
+
+    public NamedColor(Color color, String name) {
+      this.color = color;
+      this.name = name;
+    }
+
+    public Color getColor() {
+      return color;
+    }
+
+    @Override
+    public String toString() {
+      String msg = MessageFormat.format("{0} {1}", name, color.toRGBString());
+      return msg;
+    }
+  }
 }
