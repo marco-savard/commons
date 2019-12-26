52876a7d9ed0ab54f3584751bb2a64dcd4fa887a
Marco Savard <savard.marco@gmail.com>
2019-12-26 10:51:43 -0500

    Added Educational demo

--- commons/src/main/java/com/marcosavard/commons/geog/ca/qc/educ/EducationalNetworkDemo.java
+++ commons/src/main/java/com/marcosavard/commons/geog/ca/qc/educ/EducationalNetworkDemo.java
@@ -5,129 +5,131 @@ import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import com.marcosavard.commons.geog.ca.qc.RegionAdministrative;
 import com.marcosavard.commons.io.IndentWriter;
 import com.marcosavard.commons.text.WordDistance;
 
 public class EducationalNetworkDemo {
-	
-	public static void main(String[] args) {
-		findNearingSchool("G3E2C7");
-		printNetwork();
-	}
-	
-	public static void findNearingSchool(String postalCode) {
-		String code = "G3E2C7";
-		EducationalNetwork network = EducationalNetworkReader.read(); 
-		List<Organization> quebecSchools = network.getAllOrganizations();
-		
-		List<String> postalCodes = getListPostalCodes(quebecSchools);
-		String schoolPostalCode = WordDistance.findNearestString(code, postalCodes);
-		
-		Organization school = findSchoolByPostalCode(quebecSchools, schoolPostalCode);
-		String msg = MessageFormat.format("L''ecole la plus proche de {0} est : {1}", code, school);
-		System.out.println(msg);
-	}
 
-	private static List<String> getListPostalCodes(List<Organization> quebecSchools) {
-		List<String> postalCodes = quebecSchools.stream().map(s -> s.getPostalCode()).collect(Collectors.toList());
-		Collections.sort(postalCodes);
-		return postalCodes;
-	}
-	
-	private static Organization findSchoolByPostalCode(List<Organization> quebecSchools, String postalCode) {
-		Organization school = quebecSchools.stream().filter(s -> s.getPostalCode().equals(postalCode)).findFirst().get(); 
-		return school;
-	}
-	
-	private static void printNetwork() {
-		PrintWriter pw = new PrintWriter(System.out);
-		IndentWriter iw = new IndentWriter(pw); 
-		
-		EducationalNetwork network = EducationalNetworkReader.read(); 
-		printUniversityNetwork(iw, network); 
-		printCollegialNetwork(iw, network); 
-		printGeneralNetwork(iw, network); 
-		iw.flush();
-	}
+  public static void main(String[] args) {
+    findNearingSchool("G3E2C7");
+    printNetwork();
+  }
 
-	private static void printUniversityNetwork(IndentWriter iw, EducationalNetwork network) {
-		iw.println("R�seau universitaire:");
-		iw.indent(); 
-		
-		List<UniversitySystem> systems = network.getUniversitySystems();
-		for (UniversitySystem system : systems) {
-			printOrganization(iw, system); 
-		}
-		
-		List<University> universities = network.getUniversities();
-		for (University university : universities) {
-			if ("".equals(university.getParentCode())) {
-				printOrganization(iw, university); 
-			}
-		}
-		
-		iw.unindent(); 
-	}
+  public static void findNearingSchool(String postalCode) {
+    String code = "G3E2C7";
+    EducationalNetwork network = EducationalNetworkReader.read();
+    List<Organization> quebecSchools = network.getAllOrganizations();
 
-	private static void printCollegialNetwork(IndentWriter iw, EducationalNetwork network) {
-		iw.println("R�seau coll�giale:");
-		iw.indent(); 
-		
-		List<College> colleges = network.getColleges();
-		for (College college : colleges) {
-			printOrganization(iw, college); 
-		}
-		
-		iw.unindent(); 
-	}
+    List<String> postalCodes = getListPostalCodes(quebecSchools);
+    String schoolPostalCode = WordDistance.findNearestString(code, postalCodes);
 
-	private static void printGeneralNetwork(IndentWriter iw, EducationalNetwork network) {
-		iw.println("R�seau scolaire:");
-		iw.indent(); 
-				
-		for (RegionAdministrative region : RegionAdministrative.values()) {
-			iw.println(region);
-			iw.indent(); 
-			
-			List<School> schools = network.getSchoolPrivateSchoolByRegion(region); 
-			List<SchoolBoard> schoolBoards = network.getSchoolBoardsByRegion(region); 
-			
-			if (! schools.isEmpty()) {
-				iw.println("Ecoles priv�es et gouvernementales");
-				iw.indent(); 
-				
-				for (School school : schools) {
-					printOrganization(iw, school); 
-				}
-				
-				iw.unindent();
-			}
-			
-			for (SchoolBoard schoolBoard : schoolBoards) {
-				printOrganization(iw, schoolBoard); 
-			}
-						
-			iw.unindent(); 
-		}
-		
-		iw.unindent(); 
-	}
+    Organization school = findSchoolByPostalCode(quebecSchools, schoolPostalCode);
+    String msg = MessageFormat.format("L''ecole la plus proche de {0} est : {1}", code, school);
+    System.out.println(msg);
+  }
 
-	private static void printOrganization(IndentWriter iw, Organization org) {
-		iw.println(org.toDisplayString());
-		
-		List<Organization> components = org.getComponents(); 
-		if (! components.isEmpty()) {
-			iw.indent(); 
-			
-			for (Organization component : components) {
-				printOrganization(iw, component); 
-			}
-			
-			iw.unindent(); 
-		}
-	}
+  private static List<String> getListPostalCodes(List<Organization> quebecSchools) {
+    List<String> postalCodes =
+        quebecSchools.stream().map(s -> s.getPostalCode()).collect(Collectors.toList());
+    Collections.sort(postalCodes);
+    return postalCodes;
+  }
+
+  private static Organization findSchoolByPostalCode(List<Organization> quebecSchools,
+      String postalCode) {
+    Organization school =
+        quebecSchools.stream().filter(s -> s.getPostalCode().equals(postalCode)).findFirst().get();
+    return school;
+  }
+
+  private static void printNetwork() {
+    PrintWriter pw = new PrintWriter(System.out);
+    IndentWriter iw = new IndentWriter(pw);
+
+    EducationalNetwork network = EducationalNetworkReader.read();
+    printUniversityNetwork(iw, network);
+    printCollegialNetwork(iw, network);
+    printGeneralNetwork(iw, network);
+    iw.flush();
+  }
+
+  private static void printUniversityNetwork(IndentWriter iw, EducationalNetwork network) {
+    iw.println("R�seau universitaire:");
+    iw.indent();
+
+    List<UniversitySystem> systems = network.getUniversitySystems();
+    for (UniversitySystem system : systems) {
+      printOrganization(iw, system);
+    }
+
+    List<University> universities = network.getUniversities();
+    for (University university : universities) {
+      if ("".equals(university.getParentCode())) {
+        printOrganization(iw, university);
+      }
+    }
+
+    iw.unindent();
+  }
+
+  private static void printCollegialNetwork(IndentWriter iw, EducationalNetwork network) {
+    iw.println("R�seau coll�giale:");
+    iw.indent();
+
+    List<College> colleges = network.getColleges();
+    for (College college : colleges) {
+      printOrganization(iw, college);
+    }
+
+    iw.unindent();
+  }
+
+  private static void printGeneralNetwork(IndentWriter iw, EducationalNetwork network) {
+    iw.println("R�seau scolaire:");
+    iw.indent();
+
+    for (RegionAdministrative region : RegionAdministrative.values()) {
+      iw.println(region);
+      iw.indent();
+
+      List<School> schools = network.getSchoolPrivateSchoolByRegion(region);
+      List<SchoolBoard> schoolBoards = network.getSchoolBoardsByRegion(region);
+
+      if (!schools.isEmpty()) {
+        iw.println("Ecoles priv�es et gouvernementales");
+        iw.indent();
+
+        for (School school : schools) {
+          printOrganization(iw, school);
+        }
+
+        iw.unindent();
+      }
+
+      for (SchoolBoard schoolBoard : schoolBoards) {
+        printOrganization(iw, schoolBoard);
+      }
+
+      iw.unindent();
+    }
+
+    iw.unindent();
+  }
+
+  private static void printOrganization(IndentWriter iw, Organization org) {
+    iw.println(org.toDisplayString());
+
+    List<Organization> components = org.getComponents();
+    if (!components.isEmpty()) {
+      iw.indent();
+
+      for (Organization component : components) {
+        printOrganization(iw, component);
+      }
+
+      iw.unindent();
+    }
+  }
 
 }
--- commons/src/main/java/com/marcosavard/commons/io/FileSystem.java
+++ commons/src/main/java/com/marcosavard/commons/io/FileSystem.java
@@ -1,146 +1,174 @@
 package com.marcosavard.commons.io;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
-
+import java.nio.file.Paths;
 import javax.swing.JFileChooser;
 
 /**
- * A facade class that returns commonly used folders. See FileSystemDemo. 
+ * A facade class that returns commonly used folders. See FileSystemDemo.
  * 
  * @author Marco
  *
  */
 public class FileSystem {
-	
-	/**
-	 * Return the user folder, generally C:\Users\MyName on Windows
-	 * 
-	 * @return the user folder
-	 */
-	public static File getUserFolder() {
-		File homeFolder = new File(System.getProperty("user.home"));
-		return homeFolder;
-	}
-	
-	/**
-	 * Return the folder for user's documents, generally C:\Users\MyName\Documents on Windows
-	 * 
-	 * @return the user folder
-	 */
-	public static File getUserDocumentFolder() {
-		String folderName = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
-		File documentFolder = new File(folderName);
-		return documentFolder;
-	}
 
-	/**
-	 * Return the folder for user's desktop, generally C:\Users\MyName\Desktop on Windows
-	 * 
-	 * @return the user folder
-	 */
-	public static File getUserDesktopFolder() {
-		String folderName = new JFileChooser().getFileSystemView().getHomeDirectory().toString();
-		File documentFolder = new File(folderName);
-		return documentFolder;
-	}
+  /**
+   * Return the user folder, generally C:\Users\MyName on Windows
+   * 
+   * @return the user folder
+   */
+  public static File getUserFolder() {
+    File homeFolder = new File(System.getProperty("user.home"));
+    return homeFolder;
+  }
 
-	/**
-	 * Return the folder for user's download folder, generally C:\Users\MyName\Downloads on Windows
-	 * 
-	 * @return the user folder
-	 */
-	public static File getUserDownloadFolder() {
-		File homeFolder = new File(System.getProperty("user.home"));
-		File downloadFolder = new File(homeFolder, "Downloads"); 
-		return downloadFolder;
-	}
+  /**
+   * Return the folder for user's documents, generally C:\Users\MyName\Documents on Windows
+   * 
+   * @return the user folder
+   */
+  public static File getUserDocumentFolder() {
+    String folderName = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
+    File documentFolder = new File(folderName);
+    return documentFolder;
+  }
 
-	/**
-	 * Return the folder for user's music folder. 
-	 * 
-	 * @return the user folder
-	 */
-	public static File getUserMusicFolder() {
-		File userDocumentFolder = getUserDocumentFolder(); 
-		File musicFolder;
-		
-		if (SystemInfo.isWindows()) {
-			//get Music folder, sibling of Documents
-			File parent = userDocumentFolder.getParentFile();
-			musicFolder = new File(parent, "Music"); 
-		} else {
-			musicFolder = userDocumentFolder; 
-		}
+  /**
+   * Return the folder for user's desktop, generally C:\Users\MyName\Desktop on Windows
+   * 
+   * @return the user folder
+   */
+  public static File getUserDesktopFolder() {
+    String folderName = new JFileChooser().getFileSystemView().getHomeDirectory().toString();
+    File documentFolder = new File(folderName);
+    return documentFolder;
+  }
 
-		return musicFolder;
-	}
+  /**
+   * Return the folder for user's download folder, generally C:\Users\MyName\Downloads on Windows
+   * 
+   * @return the user folder
+   */
+  public static File getUserDownloadFolder() {
+    File homeFolder = new File(System.getProperty("user.home"));
+    File downloadFolder = new File(homeFolder, "Downloads");
+    return downloadFolder;
+  }
 
-	/**
-	 * Return the folder for user's pictures folder. 
-	 * 
-	 * @return the user folder
-	 */
-	public static File getUserPictureFolder() {
-		File userDocumentFolder = getUserDocumentFolder(); 
-		File pictureFolder;
-		
-		if (SystemInfo.isWindows()) {
-			//get Pictures folder, sibling of Documents
-			File parent = userDocumentFolder.getParentFile();
-			pictureFolder = new File(parent, "Pictures"); 
-		} else {
-			pictureFolder = userDocumentFolder; 
-		}
+  /**
+   * Return the folder for user's music folder.
+   * 
+   * @return the user folder
+   */
+  public static File getUserMusicFolder() {
+    File userDocumentFolder = getUserDocumentFolder();
+    File musicFolder;
 
-		return pictureFolder;
-	}
-	
-	/**
-	 * Return a folder to store temporary files.
-	 * 
-	 * @return the temporary folder
-	 */
-	public static File getTemporaryFolder() {
-		File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
-		return tmpFolder;
-	}
-	
-	/**
-	 * Return the folder where Java is installed. 
-	 * 
-	 * @return the Java home folder
-	 */
-	public static File getJreFolder() {
-		File jreFolder = new File(System.getProperty("java.home"));
-		return jreFolder;
-	}
+    if (SystemInfo.isWindows()) {
+      // get Music folder, sibling of Documents
+      File parent = userDocumentFolder.getParentFile();
+      musicFolder = new File(parent, "Music");
+    } else {
+      musicFolder = userDocumentFolder;
+    }
 
-	/**
-	 * Return the current working folder.  
-	 * 
-	 * @return the current working folder folder
-	 */
-	public static File getCurrentWorkingFolder() {
-		File homeFolder = new File(System.getProperty("user.dir"));
-		return homeFolder;
-	}
-	
-	/**
-	 * Return the location of this class.
-	 * 
-	 * @param claz (default value FileSystem.class)
-	 * @return the location of this class.
-	 */
-	public static File getClassFolder(Class claz) {
-		String className = claz.getSimpleName() + ".class"; 		
-		URL url = FileSystem.class.getResource(className); 
-		File file = new File(url.getFile());
-		File folder = file.getParentFile(); 
-		return folder;
-	}
+    return musicFolder;
+  }
 
-	public static File getClassFolder() {
-		return getClassFolder(FileSystem.class); 
-	}
+  /**
+   * Return the folder for user's pictures folder.
+   * 
+   * @return the user folder
+   */
+  public static File getUserPictureFolder() {
+    File userDocumentFolder = getUserDocumentFolder();
+    File pictureFolder;
+
+    if (SystemInfo.isWindows()) {
+      // get Pictures folder, sibling of Documents
+      File parent = userDocumentFolder.getParentFile();
+      pictureFolder = new File(parent, "Pictures");
+    } else {
+      pictureFolder = userDocumentFolder;
+    }
+
+    return pictureFolder;
+  }
+
+  /**
+   * Return a folder to store temporary files.
+   * 
+   * @return the temporary folder
+   */
+  public static File getTemporaryFolder() {
+    File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
+    return tmpFolder;
+  }
+
+  /**
+   * Return the folder where Java is installed.
+   * 
+   * @return the Java home folder
+   */
+  public static File getJreFolder() {
+    File jreFolder = new File(System.getProperty("java.home"));
+    return jreFolder;
+  }
+
+  /**
+   * Return the current working folder.
+   * 
+   * @return the current working folder folder
+   */
+  public static File getCurrentWorkingFolder() {
+    File homeFolder = new File(System.getProperty("user.dir"));
+    return homeFolder;
+  }
+
+  /**
+   * Return the location of this class.
+   * 
+   * @param claz (default value FileSystem.class)
+   * @return the location of this class.
+   */
+  public static File getClassFolder(Class claz) {
+    String className = claz.getSimpleName() + ".class";
+    URL url = FileSystem.class.getResource(className);
+    File file = new File(url.getFile());
+    File folder = file.getParentFile();
+    return folder;
+  }
+
+  public static File getClassFolder() {
+    return getClassFolder(FileSystem.class);
+  }
+
+  public static File getRootFolder(Class entity) {
+    File rootFolder;
+
+    try {
+      ClassLoader loader = entity.getClassLoader();
+      URL url = loader.getResource(".");
+      File file = Paths.get(url.toURI()).toFile();
+      rootFolder = file.getParentFile();
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+      rootFolder = null;
+    }
+
+    return rootFolder;
+  }
+
+  public static File getPackageFolder(File sourceFolder, String packageName) {
+    File packageFolder = sourceFolder;
+    String[] paths = packageName.split("\\.");
+
+    for (int i = 0; i < paths.length; i++) {
+      packageFolder = new File(packageFolder, paths[i]);
+    }
+
+    return packageFolder;
+  }
 }
