8ea515b5c5d05ac8f0766fc60b0cb92709ddcc94
Marco Savard <savard.marco@gmail.com>
2020-01-13 21:27:54 -0500

    Added LanguageGusser

--- commons/src/main/java/com/marcosavard/commons/text/LanguageGuesser.java
+++ commons/src/main/java/com/marcosavard/commons/text/LanguageGuesser.java
@@ -3,7 +3,6 @@ package com.marcosavard.commons.text;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -13,22 +12,6 @@ public class LanguageGuesser {
   public LanguageGuesser() {
     letterFrequency = new LetterFrequency();
   }
-
-  private static final Double[] ENGLISH_FREQUENCY = new Double[] { //
-      8.55, 1.60, 3.16, 3.87, 12.10, 2.18, 2.09, 4.96, 7.33, 0.22, // A-J
-      0.81, 4.21, 2.53, 7.17, 07.47, 2.07, 0.10, 6.33, 6.73, 8.94, // K-T
-      2.28, 1.06, 1.83, 0.19, 01.72, 0.11 // U-Z
-  };
-  private static final Double[] FRENCH_FREQUENCY = new Double[] { //
-      7.64, 0.90, 3.26, 3.67, 14.72, 1.07, 0.87, 0.74, 7.53, 0.61, // A-J
-      0.07, 5.46, 2.97, 7.01, 05.77, 2.52, 1.37, 6.69, 7.95, 7.24, // K-T
-      6.31, 1.84, 0.05, 0.43, 00.82, 0.37 // U-Z
-  };
-
-  // private static final LanguageLetterFrequency[] LANGAGES = new LanguageLetterFrequency[] { //
-  // new LanguageLetterFrequency(Locale.ENGLISH, ENGLISH_FREQUENCY), //
-  // new LanguageLetterFrequency(Locale.FRENCH, FRENCH_FREQUENCY), //
-  // };
 
   public Map<String, Double> getCandidateLanguages(Map<Character, Double> sampleFrequency) {
     Map<String, Double> candidates = new LinkedHashMap<>();
@@ -57,23 +40,6 @@ public class LanguageGuesser {
     return result;
   }
 
-  private String guess(Map<Character, Double> sampleFrequency) {
-    String guessed = "?";
-    double lowestDivergence = Double.MAX_VALUE;
-    Map<String, Map<Character, Double>> frequencies = letterFrequency.getFrequencyOfAllLanguages();
-
-    for (String languageCode : frequencies.keySet()) {
-      Map<Character, Double> languageFrequency = frequencies.get(languageCode);
-      double divergence = computeDivergence(sampleFrequency, languageFrequency);
-      if (divergence < lowestDivergence) {
-        lowestDivergence = divergence;
-        guessed = languageCode;
-      }
-    }
-
-    return guessed;
-  }
-
   private double computeDivergence(Map<Character, Double> sampleFrequencies,
       Map<Character, Double> languageFrequencies) {
     double divergence = 0.0;
@@ -87,22 +53,5 @@ public class LanguageGuesser {
 
     return divergence;
   }
-
-  private static class LanguageLetterFrequency {
-    private Locale language;
-    private Double[] letterFrequency;
-
-    public LanguageLetterFrequency(Locale language, Double[] letterFrequency) {
-      this.language = language;
-      this.letterFrequency = letterFrequency;
-    }
-
-    public Double[] getLetterFrequency() {
-      return letterFrequency;
-    }
-
-  }
-
-
 
 }
