e8abd8007bb633e773c1be8915f8ab97b4838768
Marco Savard <savard.marco@gmail.com>
2020-01-18 10:52:39 -0500

    Added astro classes

---
+++ commons/src/main/java/com/marcosavard/commons/astro/AstroDates.java
@@ -0,0 +1,100 @@
+package com.marcosavard.commons.astro;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.JulianFields;
+import java.util.Date;
+
+public class AstroDates {
+  public static final long ONE_DAY = 24 * 60 * 60 * 1000L;
+  public static final double JULIAN_DAY_Y2K_AT_NOON_UTC = 2451545.0;
+
+  public static int getDayOfYear(LocalDate date) {
+    return date.getDayOfYear();
+  }
+
+  public static int getDayOfYear(Date date) {
+    int year = date.getYear() + 1900;
+    int month = date.getMonth() + 1;
+    int day = date.getDate();
+    int n1 = (int) Math.floor((275.0 * month) / 9.0);
+    int n2 = (int) Math.floor((month + 9) / 12.0);
+    int n3 = (int) (1 + Math.floor((year - 4.0 * Math.floor(year / 4.0) + 2.0) / 3.0));
+    int dayOfYear = n1 - (n2 * n3) + day - 30;
+    return dayOfYear;
+  }
+
+  public static long toJulianDayNumber(ZonedDateTime dateTime) {
+    long julianDay = JulianFields.JULIAN_DAY.getFrom(dateTime);
+    return julianDay;
+  }
+
+  public static long toJulianDayNumber(Date date) {
+    int offset = date.getTimezoneOffset();
+    Date utcDate = date;
+
+    if (offset > 0) {
+      long offsetInMillis = offset * 60L * 1000L;
+      utcDate = new Date(date.getTime() + offsetInMillis);
+    }
+
+    int year = utcDate.getYear() + 1900;
+    int month = utcDate.getMonth() + 1;
+    long dayOfMonth = utcDate.getDate();
+
+    int a = (14 - month) / 12;
+    int y = year + 4800 - a;
+    int m = month + (12 * a) - 3;
+    long julianDayNumber = y * 365 + (y / 4) - (y / 100) + (y / 400) - 32045;
+    julianDayNumber += (153 * m + 2) / 5;
+    julianDayNumber += dayOfMonth;
+    return julianDayNumber;
+  }
+
+  public static double toJulianDay(ZonedDateTime dateTime) {
+    long julianDayNumber = toJulianDayNumber(dateTime);
+    int hour = dateTime.getHour();
+    int minute = dateTime.getMinute();
+    int second = dateTime.getSecond();
+    double julianDay = computeJulianDay(julianDayNumber, hour, minute, second);
+    return julianDay;
+  }
+
+  public static double toJulianDay(Date date) {
+    long julianDayNumber = toJulianDayNumber(date);
+    long offsetInMinutes = date.getTimezoneOffset();
+    Date utcDate = date;
+
+    if (offsetInMinutes > 0) {
+      long offsetInMillis = offsetInMinutes * 60L * 1000L;
+      utcDate = new Date(date.getTime() + offsetInMillis);
+    }
+
+    int hour = utcDate.getHours();
+    int minute = utcDate.getMinutes();
+    int second = utcDate.getSeconds();
+    double julianDay = computeJulianDay(julianDayNumber, hour, minute, second);
+    return julianDay;
+  }
+
+  private static double computeJulianDay(long julianDayNumber, int hour, int minute, int second) {
+    double julianDay = julianDayNumber;
+    julianDay += (hour - 12) / 24.0;
+    julianDay += (minute / 1440.0);
+    julianDay += (second / 86400.0);
+    return julianDay;
+  }
+
+  public static LocalDate toLocalDate(Date date) {
+    return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+  }
+
+  public static Date toDate(LocalDate localDate) {
+    return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+  }
+
+
+
+}
---
+++ commons/src/test/java/com/marcosavard/commons/astro/AstroDatesTest.java
@@ -0,0 +1,83 @@
+package com.marcosavard.commons.astro;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import org.junit.Test;
+import junit.framework.Assert;
+
+public class AstroDatesTest {
+
+  @Test
+  public void testJulianZonedDateUnixEra() {
+    LocalDateTime unixEra = LocalDateTime.of(1970, Month.JANUARY.getValue(), 1, 0, 0, 0);
+    testJulianDate(unixEra, 2440587.5);
+  }
+
+  @Test
+  public void testJulianZonedDateY2K() {
+    LocalDateTime y2k = LocalDateTime.of(2000, Month.JANUARY.getValue(), 1, 18, 0, 0);
+    testJulianDate(y2k, 2451545.25);
+  }
+
+  @Test
+  public void testDateUnixEra() {
+    int year = 1970 - 1900;
+    int month = 0;
+    int day = 1;
+    Date unixEra = new Date(year, month, day);
+    long offsetInMinutes = unixEra.getTimezoneOffset();
+    double decimalOffset = (offsetInMinutes / 60.0) / 24.0;
+    double expected = 2440587.5 + decimalOffset;
+    testJulianDate(unixEra, expected);
+  }
+
+  @Test
+  public void testDateUnixEra2() {
+    long secondsSinceUnixEra = 0L;
+    Date y2k = new Date(secondsSinceUnixEra);
+    testJulianDate(y2k, 2440587.5);
+  }
+
+  @Test
+  public void testJulianDateY2K() {
+    int year = 2000 - 1900;
+    int month = 0;
+    int day = 1;
+    Date y2k = new Date(year, month, day, 18, 0, 0);
+    long offsetInMinutes = y2k.getTimezoneOffset();
+    double decimalOffset = (offsetInMinutes / 60.0) / 24.0;
+    double expected = 2451545.25 + decimalOffset;
+    testJulianDate(y2k, expected);
+  }
+
+  private void testJulianDate(LocalDateTime localDateTime, double expected) {
+    ZoneId utc = ZoneOffset.UTC;
+    double delta = 0.001;
+    ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, utc);
+
+    double result = AstroDates.toJulianDay(zonedDateTime);
+
+    printResults(expected, result);
+    Assert.assertEquals(expected, result, delta);
+  }
+
+  private void printResults(double expected, double result) {
+    System.out.println("expected : " + expected);
+    System.out.println("result : " + result);
+    System.out.println();
+  }
+
+  private void testJulianDate(Date date, double expected) {
+    double delta = 0.001;
+    double result = AstroDates.toJulianDay(date);
+
+    printResults(expected, result);
+    Assert.assertEquals(expected, result, delta);
+  }
+
+}
+
