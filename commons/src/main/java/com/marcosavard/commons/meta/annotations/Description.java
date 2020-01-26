174691f756d396579e0ce4b4cf6b019d6294524d
Marco Savard <savard.marco@gmail.com>
2020-01-26 13:24:35 -0500

    Added annotations

---
+++ commons/src/main/java/com/marcosavard/commons/meta/annotations/Description.java
@@ -0,0 +1,14 @@
+package com.marcosavard.commons.meta.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface Description {
+
+  String value();
+
+}
