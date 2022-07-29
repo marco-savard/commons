02c897fe44c26e1fd794141896d3f43352fda845
marco-savard <savard.marco@gmail.com>
2022-07-29 10:30:37 -0400

    improved TreeNodeDemo

--- commons/src/main/java/com/marcosavard/commons/util/collection/TreeNodeDemo.java
+++ commons/src/main/java/com/marcosavard/commons/util/collection/TreeNodeDemo.java
@@ -1,23 +1,26 @@
 package com.marcosavard.commons.util.collection;
 
 import com.marcosavard.commons.debug.Console;
+import com.marcosavard.commons.io.TreeWriter;
+
+import java.io.StringWriter;
 
 public class TreeNodeDemo {
 
     public static void main(String[] args) {
         TreeNode usa = TreeNode.createRoot("USA");
         TreeNode west = usa.addChild("West");
-        TreeNode ca = west.addChild("CA");
-        TreeNode or = west.addChild("OR");
-        TreeNode wa = west.addChild("WA");
+        west.addChild("CA");
+        west.addChild("OR");
+        west.addChild("WA");
 
         TreeNode east = usa.addChild("East");
         TreeNode newEngland = east.addChild("newEngland");
-        TreeNode ma = newEngland.addChild("MA");
-        TreeNode ct = newEngland.addChild("CT");
-        TreeNode ri = newEngland.addChild("RI");
-        TreeNode me = newEngland.addChild("ME");
-        TreeNode vt = newEngland.addChild("VT");
+        newEngland.addChild("MA");
+        newEngland.addChild("CT");
+        newEngland.addChild("RI");
+        newEngland.addChild("ME");
+        newEngland.addChild("VT");
         TreeNode nh = newEngland.addChild("NH");
 
         Console.println(usa.toNestedString());
@@ -25,5 +28,11 @@ public class TreeNodeDemo {
         Console.println(usa.isLeaf());
         Console.println(usa.getLevel());
         Console.println(nh.getLevel());
+
+        //print a tree structure
+        StringWriter sw = new StringWriter();
+        TreeWriter tw = new TreeWriter(sw);
+        tw.write(usa);
+        Console.println(sw.toString());
     }
 }
