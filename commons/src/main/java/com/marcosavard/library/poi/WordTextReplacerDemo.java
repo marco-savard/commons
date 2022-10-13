2ee51067068c2545a31c25a726c9246dd22e5819
marco-savard <savard.marco@gmail.com>
2022-10-13 10:11:32 -0400

    improved WordTextReplacerDemo

--- commons/src/main/java/com/marcosavard/library/poi/WordTextReplacerDemo.java
+++ commons/src/main/java/com/marcosavard/library/poi/WordTextReplacerDemo.java
@@ -1,8 +1,13 @@
 package com.marcosavard.library.poi;
 
+import com.marcosavard.commons.debug.Console;
 import com.marcosavard.commons.io.FileSystem;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,31 +15,34 @@ import java.util.Locale;
 
 public class WordTextReplacerDemo {
 
-    public static void main(String args[]) throws Exception {
-        File docFolder = FileSystem.getUserDocumentFolder();
-        File wordFolder = new File(docFolder, "Documents Word");
-        String filename = "Mise à jour du questionnaire";
-        File src = new File(wordFolder, filename + ".docx");
-        File dest = new File(wordFolder, MessageFormat.format("{0}.v2.docx", filename));
+  public static void main(String[] args) throws Exception {
+    // read source
+    String resourceName = "Mise à jour du questionnaire.docx";
+    InputStream input = WordTextReplacerDemo.class.getResourceAsStream(resourceName);
 
-        if (dest.exists()) {
-            dest.delete();
-        }
+    // create target
+    File docFolder = FileSystem.getUserDocumentFolder();
+    File wordFolder = new File(docFolder, "Documents Word");
+    String filename = "Mise à jour du questionnaire";
+    File dest = new File(wordFolder, MessageFormat.format("{0}.v2.docx", filename));
 
-        InputStream in = new FileInputStream(src);
-        OutputStream out = new FileOutputStream(dest);
-
-        LocalDate today = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy", Locale.FRENCH);
-        String dateToday = formatter.format(today);
-
-        WordTextReplacer replacer = new WordTextReplacer(in, out);
-        replacer.addReplacement("enfant.nom", "Savard");
-        replacer.addReplacement("enfant.prenom", "Sarah-Maude");
-        replacer.addReplacement("date.naissance", "22 Septembre 2011");
-        replacer.addReplacement("code.ramq", "SAVS 1109 2201");
-        replacer.addReplacement("enseignant.nom", "Prof Untel");
-        replacer.addReplacement("date.today", dateToday);
-        replacer.replace();
+    if (dest.exists()) {
+      Files.delete(dest.toPath());
     }
+
+    LocalDate today = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy", Locale.FRENCH);
+    String dateToday = formatter.format(today);
+
+    OutputStream output = new FileOutputStream(dest);
+    WordTextReplacer replacer = new WordTextReplacer(input, output);
+    replacer.addReplacement("enfant.nom", "Savard");
+    replacer.addReplacement("enfant.prenom", "Sarah-Maude");
+    replacer.addReplacement("date.naissance", "22 Septembre 2011");
+    replacer.addReplacement("code.ramq", "SAVS 1109 2201");
+    replacer.addReplacement("enseignant.nom", "Prof Untel");
+    replacer.addReplacement("date.today", dateToday);
+    replacer.replace();
+    Console.println("File {0} generated", dest.getPath());
+  }
 }
