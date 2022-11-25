0d4d375817843216f3c278cb4425a1b5ca2526ea
Marco Savard <savard.marco@gmail.com>
2022-11-25 14:08:44 -0500

    rename controllers

--- commons/src/main/java/com/marcosavard/webapp/controller/MetricResultController.java
+++ commons/src/main/java/com/marcosavard/webapp/controller/MetricResultController.java
@@ -10,16 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class FileInfoController {
+public class MetricResultController {
     @Autowired
     private FileInfoService fileInfoService;
 
-    @GetMapping("/fileinfo")
+    @GetMapping("/metric/result")
     public String fileinfo(HttpServletRequest request, Model model) {
         FileData fileData = fileInfoService.getFileData();
         model.addAttribute("fileData", fileData);
 
-
-        return "fileinfo";
+        return "metric-result";
     }
 }
