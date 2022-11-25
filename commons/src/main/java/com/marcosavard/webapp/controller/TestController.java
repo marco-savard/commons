022f33bf5f35fe5255b3c25f0fda4aad3548497a
Marco Savard <savard.marco@gmail.com>
2022-11-25 10:00:06 -0500

    added web app

---
+++ commons/src/main/java/com/marcosavard/webapp/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.marcosavard.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class TestController {
+    private static final String TIME_PATTERN  = "yyyy/MMM/dd hh:mm:ss";
+    private String startTime;
+
+    public TestController() {
+        LocalDateTime now = LocalDateTime.now();
+        startTime = DateTimeFormatter.ofPattern(TIME_PATTERN).format(now);
+    }
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+        String responseTime = DateTimeFormatter.ofPattern(TIME_PATTERN).format(now);
+        model.addAttribute("startTime", startTime);
+        model.addAttribute("responseTime", responseTime);
+        return "test";
+    }
+}
