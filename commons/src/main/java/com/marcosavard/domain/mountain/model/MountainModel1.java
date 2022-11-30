d7c9070f99cc0a8e3fe280cee21ccd4f3c0db7f9
Marco Savard <savard.marco@gmail.com>
2022-11-30 15:20:34 -0500

    improved pojogen

--- commons/src/main/java/com/marcosavard/domain/mountain/model/MountainModel1.java
+++ commons/src/main/java/com/marcosavard/domain/mountain/model/MountainModel1.java
@@ -3,7 +3,6 @@ package com.marcosavard.domain.mountain.model;
 import java.util.Optional;
 
 public class MountainModel1 {
-
     public static class Mountain {
         public final String name = "Unnamed";
         public double latitude, longitude;
