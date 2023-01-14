6d453f8d55f7897bee2b7c44ea8827fd83daf487
marco-savard <savard.marco@gmail.com>
2023-01-13 19:08:53 -0500

    improved commons

--- commons/src/main/java/com/marcosavard/commons/util/ToStringBuilderDemo.java
+++ commons/src/main/java/com/marcosavard/commons/util/ToStringBuilderDemo.java
@@ -3,12 +3,11 @@ package com.marcosavard.commons.util;
 import java.util.Date;
 
 public class ToStringBuilderDemo {
-	
-	public static void main(String[] args) {
-		//LocalDate date =  LocalDate.now();
-		Date date = new Date();
-		String str = ToStringBuilder.build(date); 
-		System.out.println(str);
-	}
 
+  public static void main(String[] args) {
+    // LocalDate date =  LocalDate.now();
+    Date date = new Date();
+    String str = ToStringBuilder.build(date);
+    System.out.println(str);
+  }
 }
