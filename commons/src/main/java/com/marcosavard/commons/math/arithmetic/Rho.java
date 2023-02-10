50dfeadb4a344571a62f46646f747dc28939f281
marco-savard <savard.marco@gmail.com>
2023-02-10 18:49:42 -0500

    improved commons

--- commons/src/main/java/com/marcosavard/commons/math/arithmetic/Rho.java
+++ commons/src/main/java/com/marcosavard/commons/math/arithmetic/Rho.java
@@ -8,114 +8,110 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rho {
-	List<Integer> list;
-	int[] dimensions;
+  List<Integer> list;
+  int[] dimensions;
 
-	
-	public static Rho of(Range range, int... dim) {
-		return new Rho(range, dim);
-	}
-	
-	public static Rho of(List<Integer> list, int... dim) {
-		return new Rho(list, dim);
-	}
-	
-	private Rho(List<Integer> list, int... dim) {
-		this.list = list;
-		this.dimensions = dim;
-	}
-	
-	public List<List<Integer>> toList() {
-		int len = Math.min(list.size(), product(dimensions));
-		List displayList = list.subList(0,  len); 
-		
-		for (int dim = dimensions.length - 1; dim >= 0; dim--) { 
-			displayList = split(displayList, dimensions[dim]); 
-		}
-		
-		return displayList;
-	}
-	
-	public List<List<Integer>> toListOld() {
-		for (int dimension : dimensions) {
-			List rows = split(list, dimension); 
-			
-			List row = list.subList(0, dimension-1);
-			Console.println(row);
-		}
-		Console.println(); 
-		
-		List<List<Integer>> nested = new ArrayList<>(); 
-		List<Integer> row = new ArrayList<>(); 
-		row.add(3);
-		nested.add(row); 
-		return nested;
-	}
+  public static Rho of(Range range, int... dim) {
+    return new Rho(range, dim);
+  }
 
+  public static Rho of(List<Integer> list, int... dim) {
+    return new Rho(list, dim);
+  }
 
+  private Rho(List<Integer> list, int... dim) {
+    this.list = list;
+    this.dimensions = dim;
+  }
 
-	public int[] toArray() {
-		int[] array = new int[3];
-		return array;
-	}
-	
-	@Override
-	public String toString() {
-		return toString("0");
-	}
-	
-	public String toString(String decimalFormat) {
-		int len = Math.min(list.size(), product(dimensions));
-		List displayList = list.subList(0,  len); 
-		
-		for (int dim = dimensions.length - 1; dim >= 0; dim--) { 
-			displayList = split(displayList, dimensions[dim]); 
-		}
-		
-		return toString(displayList, decimalFormat); 
-	}
-	
-	private String toString(List displayList, String pattern) {
-		DecimalFormat df = new DecimalFormat(pattern);
-		return toString(displayList, df);
-	}
-	
-	private String toString(List displayList, DecimalFormat decimalFormat) {
-		List<String> strings = new ArrayList<>();
-		
-		for (Object item : displayList) {
-			if (item instanceof List) {
-				String str = "[" + toString((List)item, decimalFormat) + "]";
-				strings.add(str);
-			} else if (item instanceof Number) {
-				Number number = (Number)item;
-				String formatted = decimalFormat.format(number); 
-				strings.add(formatted);
-			}
-		}
-		
-		return String.join(" ", strings) ;
-	}
+  public List<List<Integer>> toList() {
+    int len = Math.min(list.size(), product(dimensions));
+    List displayList = list.subList(0, len);
 
-	private int product(int[] array) {
-		int product = 1;
-		for (int dim = 0; dim < dimensions.length; dim++) { 
-			product *= dimensions[dim]; 
-		}
-		
-		return product;
-	}
+    for (int dim = dimensions.length - 1; dim >= 0; dim--) {
+      displayList = split(displayList, dimensions[dim]);
+    }
 
-	private List split(List list, int length) {
-		List<List> splitted = new ArrayList<>(); 
-		
-		int nb = (list.size() + length -1) / length; 
-		for (int i=0; i<nb; i++) {
-			List row = list.subList(i*length, Math.min((i+1)*length, list.size()));
-			splitted.add(row);
-		}
-		
-		return splitted;
-	}
+    return displayList;
+  }
 
+  public List<List<Integer>> toListOld() {
+    for (int dimension : dimensions) {
+      List rows = split(list, dimension);
+
+      List row = list.subList(0, dimension - 1);
+      Console.println(row);
+    }
+    Console.println();
+
+    List<List<Integer>> nested = new ArrayList<>();
+    List<Integer> row = new ArrayList<>();
+    row.add(3);
+    nested.add(row);
+    return nested;
+  }
+
+  public int[] toArray() {
+    int[] array = new int[3];
+    return array;
+  }
+
+  @Override
+  public String toString() {
+    return toString("0");
+  }
+
+  public String toString(String decimalFormat) {
+    int len = Math.min(list.size(), product(dimensions));
+    List displayList = list.subList(0, len);
+
+    for (int dim = dimensions.length - 1; dim >= 0; dim--) {
+      displayList = split(displayList, dimensions[dim]);
+    }
+
+    return toString(displayList, decimalFormat);
+  }
+
+  private String toString(List displayList, String pattern) {
+    DecimalFormat df = new DecimalFormat(pattern);
+    return toString(displayList, df);
+  }
+
+  private String toString(List displayList, DecimalFormat decimalFormat) {
+    List<String> strings = new ArrayList<>();
+
+    for (Object item : displayList) {
+      if (item instanceof List) {
+        String str = "[" + toString((List) item, decimalFormat) + "]";
+        strings.add(str);
+      } else if (item instanceof Number) {
+        Number number = (Number) item;
+        String formatted = decimalFormat.format(number);
+        strings.add(formatted);
+      }
+    }
+
+    return String.join(" ", strings);
+  }
+
+  private int product(int[] array) {
+    int product = 1;
+    for (int dim = 0; dim < dimensions.length; dim++) {
+      product *= dimensions[dim];
+    }
+
+    return product;
+  }
+
+  private List split(List list, int length) {
+    List<List> splitted = new ArrayList<>();
+
+    int nb = (list.size() + length - 1) / length;
+    for (int i = 0; i < nb; i++) {
+      List row = list.subList(i * length, Math.min((i + 1) * length, list.size()));
+      splitted.add(row);
+    }
+
+    return splitted;
+  }
 }
--- commons/src/main/java/com/marcosavard/commons/math/arithmetic/RomanNumeral.java
+++ commons/src/main/java/com/marcosavard/commons/math/arithmetic/RomanNumeral.java
@@ -2,30 +2,28 @@ package com.marcosavard.commons.math.arithmetic;
 
 /**
  * A class that converts numbers to roman numeral.
- * 
- * @author Marco
  *
+ * @author Marco
  */
 public class RomanNumeral {
   public static final RomanNumeral ONE = new RomanNumeral(1);
   public static final RomanNumeral TEN = new RomanNumeral(10);
 
-  private final static int[] NUMBERS = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-  private final static String[] LETTERS =
-      {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+  private static final int[] NUMBERS = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  private static final String[] LETTERS = {
+    "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+  };
   private int number;
 
   /**
    * Create a roman numeral whose toString() is its textual representation. For instance,
    * System.out.println(new RomanNumeral(10)) prints "X".
-   * 
+   *
    * @param number between 1 and 3999
    */
   public static RomanNumeral of(int number) {
-    if (number < 1)
-      throw new IllegalArgumentException("must be positive : " + number);
-    if (number > 3999)
-      throw new IllegalArgumentException("must be 3999 or less : " + number);
+    if (number < 1) throw new IllegalArgumentException("must be positive : " + number);
+    if (number > 3999) throw new IllegalArgumentException("must be 3999 or less : " + number);
 
     if (number == 1) {
       return RomanNumeral.ONE;
