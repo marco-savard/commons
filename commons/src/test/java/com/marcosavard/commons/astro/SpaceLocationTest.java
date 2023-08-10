d89973037d36d7c97fafcf485c90d0a8d6f0887f
marco-savard <savard.marco@gmail.com>
2023-08-10 13:18:47 -0400

    improved tests

--- commons/src/test/java/com/marcosavard/commons/astro/SpaceLocationTest.java
+++ commons/src/test/java/com/marcosavard/commons/astro/SpaceLocationTest.java
@@ -2,9 +2,8 @@ package com.marcosavard.commons.astro;
 
 import com.marcosavard.commons.astro.space.SpaceCoordinate;
 import com.marcosavard.commons.geog.GeoLocation;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -14,38 +13,38 @@ import static com.marcosavard.commons.geog.GeoLocation.LongitudeHemisphere.WEST;
 
 public class SpaceLocationTest {
 
-    @Test
-    public void testBirminghamUK() {
-        // position of the star M13..
-        SpaceCoordinate starM13 = StarAlmanach.M13;
+  @Test
+  public void testBirminghamUK() {
+    // position of the star M13..
+    SpaceCoordinate starM13 = StarAlmanach.M13;
 
-        // ..as seen from this location
-        GeoLocation birminghamUK = GeoLocation.of(52, 30, NORTH, 1, 55, WEST);
+    // ..as seen from this location
+    GeoLocation birminghamUK = GeoLocation.of(52, 30, NORTH, 1, 55, WEST);
 
-        // ..at this moment
-        LocalDateTime localTime = LocalDateTime.of(1998, 8, 10, 23, 10, 0); // 2310 UT, 10th August 1998
-        ZonedDateTime moment = ZonedDateTime.of(localTime, ZoneOffset.UTC);
+    // ..at this moment
+    LocalDateTime localTime = LocalDateTime.of(1998, 8, 10, 23, 10, 0); // 2310 UT, 10th August 1998
+    ZonedDateTime moment = ZonedDateTime.of(localTime, ZoneOffset.UTC);
 
-        /*
-        // get sky position
-        SkyPosition skyPosition2 =
-                Astronomy.findSkyPositionOf(starM13, moment, birminghamUK.toCoordinates());
+    /*
+    // get sky position
+    SkyPosition skyPosition2 =
+            Astronomy.findSkyPositionOf(starM13, moment, birminghamUK.toCoordinates());
 
-        SkyPosition skyPosition = Astronomy.findSkyPositionOf(starM13, moment, birminghamUK);
-        Assert.assertEquals(skyPosition.getAzimuth(), 269.146, 0.01);
-        Assert.assertEquals(skyPosition.getHorizon(), 49.169, 0.01);
+    SkyPosition skyPosition = Astronomy.findSkyPositionOf(starM13, moment, birminghamUK);
+    Assert.assertEquals(skyPosition.getAzimuth(), 269.146, 0.01);
+    Assert.assertEquals(skyPosition.getHorizon(), 49.169, 0.01);
 
-        System.out.println("Position of star M13 above Birmingham, UK on August 10st, 1998 at 23:10");
-        System.out.println("  ..position of M13: " + skyPosition);
-        System.out.println();
+    System.out.println("Position of star M13 above Birmingham, UK on August 10st, 1998 at 23:10");
+    System.out.println("  ..position of M13: " + skyPosition);
+    System.out.println();
 
-         */
+     */
 
-        /*
-        double[] coordinates = starM13.getZenithAt(moment);
-        GeoLocation location = GeoLocation.of(coordinates[0], coordinates[1]);
-        String msg = MessageFormat.format("({0}) is above ({1}) at {2}", starM13, location, moment);
-        System.out.println(msg);
-*/
-    }
+    /*
+            double[] coordinates = starM13.getZenithAt(moment);
+            GeoLocation location = GeoLocation.of(coordinates[0], coordinates[1]);
+            String msg = MessageFormat.format("({0}) is above ({1}) at {2}", starM13, location, moment);
+            System.out.println(msg);
+    */
+  }
 }
--- commons/src/test/java/com/marcosavard/commons/astro/SunTimesTest.java
+++ commons/src/test/java/com/marcosavard/commons/astro/SunTimesTest.java
@@ -1,55 +1,52 @@
 package com.marcosavard.commons.astro;
 
+import com.marcosavard.commons.astro.finder.SunTimes;
+import junit.framework.Assert;
+import org.junit.Test;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 
-
-import com.marcosavard.commons.astro.finder.SunTimes;
-
-import junit.framework.Assert;
-import org.junit.jupiter.api.Test;
-
 public class SunTimesTest {
-	
-	@Test
-	public void testWayneNJ() {
-		ZoneId est = ZoneId.of("America/New_York");
-		double[] wayneNJ = Location.of(40.9, -74.3);
 
-		SunTimes times = test(LocalDate.of(1990, 6, 25), wayneNJ, est, SunTimes.OFFICIAL_HORIZON);
-		LocalTime sunRise = times.getSunRise(); 
-		LocalTime expected = LocalTime.of(5, 26); //should give {sunrise = 5:26 am EDT}
-		assertEqual(expected, sunRise); 
-	}
-	
-	@Test
-	public void testQuebec() {
-		ZoneId est = ZoneId.of("America/New_York");
-		double[] qc = Location.of(46.8139, -71.2080);
+  @Test
+  public void testWayneNJ() {
+    ZoneId est = ZoneId.of("America/New_York");
+    double[] wayneNJ = Location.of(40.9, -74.3);
 
-		SunTimes times = test(LocalDate.of(2021, 4, 21), qc, est, SunTimes.OFFICIAL_HORIZON);
-		LocalTime sunRise = times.getSunRise(); 
-		LocalTime expected = LocalTime.of(5, 46); //should give {sunrise = 5:46 am EDT}
-		assertEqual(expected, sunRise); 
-	}
-	
-	private void assertEqual(LocalTime expectedTime, LocalTime actualTime) {
-		int expected = expectedTime.toSecondOfDay(); 
-		int actual = actualTime.toSecondOfDay(); 
-		Assert.assertEquals(expected, actual); 
-	}
+    SunTimes times = test(LocalDate.of(1990, 6, 25), wayneNJ, est, SunTimes.OFFICIAL_HORIZON);
+    LocalTime sunRise = times.getSunRise();
+    LocalTime expected = LocalTime.of(5, 26); // should give {sunrise = 5:26 am EDT}
+    assertEqual(expected, sunRise);
+  }
 
-	private SunTimes test(LocalDate date, double[] location, ZoneId zone, double angle) {
-		SunTimes times = SunTimes.of(date, location, zone, angle);
-		return times;
-	}
+  @Test
+  public void testQuebec() {
+    ZoneId est = ZoneId.of("America/New_York");
+    double[] qc = Location.of(46.8139, -71.2080);
 
-	private static class Location {
-		public static double[] of(double lat, double lon) {
-			double[] coordinates = new double[] {lat, lon};
-			return coordinates;
-		}
-	}
+    SunTimes times = test(LocalDate.of(2021, 4, 21), qc, est, SunTimes.OFFICIAL_HORIZON);
+    LocalTime sunRise = times.getSunRise();
+    LocalTime expected = LocalTime.of(5, 46); // should give {sunrise = 5:46 am EDT}
+    assertEqual(expected, sunRise);
+  }
 
+  private void assertEqual(LocalTime expectedTime, LocalTime actualTime) {
+    int expected = expectedTime.toSecondOfDay();
+    int actual = actualTime.toSecondOfDay();
+    Assert.assertEquals(expected, actual);
+  }
+
+  private SunTimes test(LocalDate date, double[] location, ZoneId zone, double angle) {
+    SunTimes times = SunTimes.of(date, location, zone, angle);
+    return times;
+  }
+
+  private static class Location {
+    public static double[] of(double lat, double lon) {
+      double[] coordinates = new double[] {lat, lon};
+      return coordinates;
+    }
+  }
 }
