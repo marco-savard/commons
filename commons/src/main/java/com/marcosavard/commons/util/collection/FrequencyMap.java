6ba705c1eb714237075a3c5c43be5a19f21b7cf3
marco-savard <savard.marco@gmail.com>
2023-09-23 19:20:12 -0400

    improved frequencyMap

--- commons/src/main/java/com/marcosavard/commons/util/collection/FrequencyMap.java
+++ commons/src/main/java/com/marcosavard/commons/util/collection/FrequencyMap.java
@@ -8,6 +8,16 @@ import java.util.List;
 import java.util.Set;
 
 public class FrequencyMap<T> extends TreeValueMap<T, Integer> {
+
+  public static <T> FrequencyMap of(List<T> items) {
+    FrequencyMap<T> map = new FrequencyMap<T>();
+    for (T item : items) {
+      map.add(item);
+    }
+
+    return map;
+  }
+
   public void add(T key) {
     if (super.containsKey(key)) {
       super.put(key, super.get(key) + 1);
@@ -27,5 +37,21 @@ public class FrequencyMap<T> extends TreeValueMap<T, Integer> {
 
     Set<T> targetSet = new LinkedHashSet<>(list);
     return targetSet;
+  }
+
+  public T getMostFrequent() {
+    Set<T> keys = super.keySet();
+    int higherFrequency = 0;
+    T mostFrequent = null;
+
+    for (T t : keys) {
+      int frequency = super.get(t);
+      if (frequency > higherFrequency) {
+        mostFrequent = t;
+        higherFrequency = frequency;
+      }
+    }
+
+    return mostFrequent;
   }
 }
