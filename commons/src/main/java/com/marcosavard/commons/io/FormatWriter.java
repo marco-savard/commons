51d20cb9cc6b76bd4c74582f3481ea6a9812cb9b
marco-savard <savard.marco@gmail.com>
2023-11-11 07:55:35 -0500

    bug fix

--- commons/src/main/java/com/marcosavard/commons/io/FormatWriter.java
+++ commons/src/main/java/com/marcosavard/commons/io/FormatWriter.java
@@ -5,27 +5,29 @@ import java.text.MessageFormat;
 
 public class FormatWriter extends IndentWriter {
 
-    public FormatWriter(Writer w, int indentation) {
-        super(w, indentation);
-    }
+  public FormatWriter(Writer w, int indentation) {
+    super(w, indentation);
+  }
 
-    public FormatWriter(Writer w) {
-        this(w, DEFAULT_INDENTATION);
-    }
+  public FormatWriter(Writer w) {
+    this(w, DEFAULT_INDENTATION);
+  }
 
-    public void print(String pattern, Object... items) {
-        super.print(MessageFormat.format(pattern, items));
-    }
+  public void print(String pattern, Object... items) {
+    pattern = pattern.replace("'", "''");
+    super.print(MessageFormat.format(pattern, items));
+  }
 
-    public void println(String pattern, Object... items) {
-        super.println(MessageFormat.format(pattern, items));
-    }
+  public void println(String pattern, Object... items) {
+    pattern = pattern.replace("'", "''");
+    super.println(MessageFormat.format(pattern, items));
+  }
 
-    public void printlnIndented(String line) {
-        super.printlnIndented(line);
-    }
+  public void printlnIndented(String line) {
+    super.printlnIndented(line);
+  }
 
-    public void printlnIndented(String pattern, Object... items) {
-        super.printlnIndented(MessageFormat.format(pattern, items));
-    }
+  public void printlnIndented(String pattern, Object... items) {
+    super.printlnIndented(MessageFormat.format(pattern, items));
+  }
 }
