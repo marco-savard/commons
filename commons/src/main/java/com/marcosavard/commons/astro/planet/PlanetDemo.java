0020622f28fd77c5a05aae4ec201d5294c6801d7
marco-savard <savard.marco@gmail.com>
2023-11-19 14:36:35 -0500

    added classes

---
+++ commons/src/main/java/com/marcosavard/commons/astro/planet/PlanetDemo.java
@@ -0,0 +1,17 @@
+package com.marcosavard.commons.astro.planet;
+
+import com.marcosavard.commons.debug.Console;
+
+import java.util.Locale;
+
+public class PlanetDemo {
+  public static void main(String[] args) {
+    Locale display = Locale.FRENCH;
+
+    for (Planet planet : Planet.values()) {
+      String name = planet.getDisplayName(display);
+      String symbol = Character.toString(planet.getSymbol());
+      Console.println("{0} : {1}", name, symbol);
+    }
+  }
+}
