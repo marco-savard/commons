29a896deaf0c96ac22d4e655283d56a3217927a1
marco-savard <savard.marco@gmail.com>
2024-01-04 10:48:49 -0500

    added classes

--- commons/src/main/java/com/marcosavard/commons/ling/Numeral.java
+++ commons/src/main/java/com/marcosavard/commons/ling/Numeral.java
@@ -1,103 +1,218 @@
 package com.marcosavard.commons.ling;
 
-public class Numeral {
-  private static final String[] DIGITS =
-      new String[] {
-        "zero",
-        "un",
-        "deux",
-        "trois",
-        "quatre",
-        "cinq",
-        "six",
-        "sept",
-        "huit",
-        "neuf",
-        "dix",
-        "onze",
-        "douze",
-        "treize",
-        "quatorze",
-        "quinze",
-        "seize"
-      };
+import java.util.Locale;
 
-  private static final String[] TENS =
-      new String[] {
-        "",
-        "dix",
-        "vingt",
-        "trente",
-        "quarante",
-        "cinquante",
-        "soixante",
-        "soixante-dix",
-        "quatre-vingt",
-        "quatre-vingt-dix",
-        "cent"
-      };
+public abstract class Numeral {
 
-  public String of(int number) {
-    String numeral;
+  public static Numeral of(Locale display) {
+    Numeral numeral;
 
-    if (number <= 16) {
-      numeral = DIGITS[number];
-    } else if (number <= 59) {
-      numeral = upTo60(number);
-    } else if (number <= 99) {
-      numeral = upTo100(number);
-    } else if (number <= 999) {
-      numeral = upTo1000(number);
-    } else if (number <= 999_999) {
-      numeral = upTo1_000_000(number);
-    } else if (number <= 99_999_999) {
-      numeral = upTo1_000_000_000(number);
+    if ("fr".equals(display.getLanguage())) {
+      numeral = new FrNumeral();
     } else {
-      numeral = Integer.toString(number);
+      numeral = new EnNumeral();
     }
 
     return numeral;
   }
 
-  private String upTo60(int number) {
-    int tens = number / 10;
-    int units = number % 10;
-    String numeral = TENS[tens];
-    numeral = (units == 1) ? numeral + "-et" : numeral;
-    numeral = (units == 0) ? numeral : numeral + "-" + DIGITS[units];
-    return numeral;
+  public abstract String getDisplayName(int number);
+
+  private static class EnNumeral extends Numeral {
+    private static final String[] DIGITS =
+        new String[] {
+          "zero",
+          "one",
+          "two",
+          "three",
+          "four",
+          "five",
+          "six",
+          "seven",
+          "eight",
+          "nine",
+          "ten",
+          "eleven",
+          "twelve",
+          "thirteen",
+          "forteen",
+          "fifteen",
+          "sixteen",
+          "seventeen",
+          "eighteen",
+          "nineteen"
+        };
+
+    private static final String[] TENS =
+        new String[] {
+          "",
+          "ten",
+          "twenty",
+          "thirty",
+          "forty",
+          "fifty",
+          "sixty",
+          "seventy",
+          "eighty",
+          "ninety",
+          "one hundred"
+        };
+
+    @Override
+    public String getDisplayName(int number) {
+      String numeral = "?";
+
+      if (number <= 19) {
+        numeral = DIGITS[number];
+      } else if (number <= 99) {
+        numeral = upTo100(number);
+      } else if (number <= 999) {
+        numeral = upTo1000(number);
+      } else if (number <= 999_999) {
+        numeral = upTo1_000_000(number);
+      } else if (number <= 99_999_999) {
+        numeral = upTo1_000_000_000(number);
+      } else {
+        numeral = Integer.toString(number);
+      }
+      return numeral;
+    }
+
+    private String upTo100(int number) {
+      int tens = (number / 10);
+      int units = number % 10;
+      String numeral = TENS[tens];
+      numeral = (units == 0) ? numeral : numeral + "-" + getDisplayName(units);
+      return numeral;
+    }
+
+    private String upTo1000(int number) {
+      int hundreds = number / 100;
+      int units = number % 100;
+      String numeral =
+          (hundreds == 1) ? "one hundred" : getDisplayName(hundreds) + " " + "hundreds";
+      numeral = (units == 0) ? numeral : numeral + " " + getDisplayName(units);
+      return numeral;
+    }
+
+    private String upTo1_000_000(int number) {
+      int thousands = number / 1000;
+      int units = number % 1000;
+      String numeral =
+          (thousands == 1) ? "one thousand" : getDisplayName(thousands) + " " + "thousands";
+      numeral = (units == 0) ? numeral : numeral + " " + getDisplayName(units);
+      return numeral;
+    }
+
+    private String upTo1_000_000_000(int number) {
+      int millions = number / 1_000_000;
+      int units = number % 1_000_000;
+      String numeral = getDisplayName(millions) + " " + "milion";
+      numeral = (units == 0) ? numeral : numeral + " " + getDisplayName(units);
+      return numeral;
+    }
   }
 
-  private String upTo100(int number) {
-    int tens = (number / 20) * 2;
-    int units = number % 20;
-    String numeral = TENS[tens];
-    numeral = (units == 1) ? numeral + "-et" : numeral;
-    numeral = (units == 0) ? numeral : numeral + "-" + of(units);
-    return numeral;
-  }
+  private static class FrNumeral extends Numeral {
+    private static final String[] DIGITS =
+        new String[] {
+          "zero",
+          "un",
+          "deux",
+          "trois",
+          "quatre",
+          "cinq",
+          "six",
+          "sept",
+          "huit",
+          "neuf",
+          "dix",
+          "onze",
+          "douze",
+          "treize",
+          "quatorze",
+          "quinze",
+          "seize"
+        };
 
-  private String upTo1000(int number) {
-    int hundreds = number / 100;
-    int units = number % 100;
-    String numeral = (hundreds == 1) ? "cent" : of(hundreds) + " " + "cents";
-    numeral = (units == 0) ? numeral : numeral + " " + of(units);
-    return numeral;
-  }
+    private static final String[] TENS =
+        new String[] {
+          "",
+          "dix",
+          "vingt",
+          "trente",
+          "quarante",
+          "cinquante",
+          "soixante",
+          "soixante-dix",
+          "quatre-vingt",
+          "quatre-vingt-dix",
+          "cent"
+        };
 
-  private String upTo1_000_000(int number) {
-    int thousands = number / 1000;
-    int units = number % 1000;
-    String numeral = (thousands == 1) ? "mille" : of(thousands) + " " + "milles";
-    numeral = (units == 0) ? numeral : numeral + " " + of(units);
-    return numeral;
-  }
+    @Override
+    public String getDisplayName(int number) {
+      String numeral;
 
-  private String upTo1_000_000_000(int number) {
-    int millions = number / 1_000_000;
-    int units = number % 1_000_000;
-    String numeral = of(millions) + " " + "million";
-    numeral = (units == 0) ? numeral : numeral + " " + of(units);
-    return numeral;
+      if (number <= 16) {
+        numeral = DIGITS[number];
+      } else if (number <= 59) {
+        numeral = upTo60(number);
+      } else if (number <= 99) {
+        numeral = upTo100(number);
+      } else if (number <= 999) {
+        numeral = upTo1000(number);
+      } else if (number <= 999_999) {
+        numeral = upTo1_000_000(number);
+      } else if (number <= 99_999_999) {
+        numeral = upTo1_000_000_000(number);
+      } else {
+        numeral = Integer.toString(number);
+      }
+
+      return numeral;
+    }
+
+    private String upTo60(int number) {
+      int tens = number / 10;
+      int units = number % 10;
+      String numeral = TENS[tens];
+      numeral = (units == 1) ? numeral + "-et" : numeral;
+      numeral = (units == 0) ? numeral : numeral + "-" + DIGITS[units];
+      return numeral;
+    }
+
+    private String upTo100(int number) {
+      int tens = (number / 20) * 2;
+      int units = number % 20;
+      String numeral = TENS[tens];
+      numeral = (units == 1) ? numeral + "-et" : numeral;
+      numeral = (units == 0) ? numeral : numeral + "-" + getDisplayName(units);
+      return numeral;
+    }
+
+    private String upTo1000(int number) {
+      int hundreds = number / 100;
+      int units = number % 100;
+      String numeral = (hundreds == 1) ? "cent" : getDisplayName(hundreds) + " " + "cents";
+      numeral = (units == 0) ? numeral : numeral + " " + getDisplayName(units);
+      return numeral;
+    }
+
+    private String upTo1_000_000(int number) {
+      int thousands = number / 1000;
+      int units = number % 1000;
+      String numeral = (thousands == 1) ? "mille" : getDisplayName(thousands) + " " + "milles";
+      numeral = (units == 0) ? numeral : numeral + " " + getDisplayName(units);
+      return numeral;
+    }
+
+    private String upTo1_000_000_000(int number) {
+      int millions = number / 1_000_000;
+      int units = number % 1_000_000;
+      String numeral = getDisplayName(millions) + " " + "million";
+      numeral = (units == 0) ? numeral : numeral + " " + getDisplayName(units);
+      return numeral;
+    }
   }
 }
--- commons/src/main/java/com/marcosavard/commons/ling/NumeralDemo.java
+++ commons/src/main/java/com/marcosavard/commons/ling/NumeralDemo.java
@@ -2,13 +2,16 @@ package com.marcosavard.commons.ling;
 
 import com.marcosavard.commons.debug.Console;
 
+import java.util.Locale;
+
 public class NumeralDemo {
 
   public static void main(String[] args) {
-    Numeral numeral = new Numeral();
+    Locale display = Locale.ENGLISH;
+    Numeral numeral = Numeral.of(display);
 
-    for (int i = 1; i <= 1_000_000; i++) {
-      Console.println("{0} : {1}", Integer.toString(i), numeral.of(i));
+    for (int i = 1; i <= 10_000; i++) {
+      Console.println("{0} : {1}", Integer.toString(i), numeral.getDisplayName(i));
     }
   }
 }
