0707d5b77290f07d616394e67b2c6d3c17eb11ce
Marco Savard <savard.marco@gmail.com>
2025-03-13 20:19:49 -0400

    improved code

--- commons/src/main/java/com/marcosavard/commons/geog/io/CountryCapitalReader.java
+++ commons/src/main/java/com/marcosavard/commons/geog/io/CountryCapitalReader.java
@@ -1,5 +1,8 @@
 package com.marcosavard.commons.geog.io;
 
+import com.marcosavard.commons.debug.Console;
+import com.marcosavard.commons.geog.Country;
+import com.marcosavard.commons.geog.GeoLocation;
 import com.marcosavard.commons.io.reader.ResourceReader;
 import com.marcosavard.commons.util.LocaleUtil;
 
@@ -35,15 +38,43 @@ public class CountryCapitalReader {
         return displayCountry;
     }
 
-    public List<String> getCapitalCities(String countryCode) {
+    public List<Country.Capital> getCapitalCities(String countryCode) {
         String[] data = readCountryData(countryCode);
         List<String> capitalCities = new ArrayList<>();
-        capitalCities.add(data[2]);
+        List<Country.Capital> capitals = new ArrayList<>();
 
-        if (data.length > 3) {
-            capitalCities.add(data[3]);
+        if (! List.of("AQ", "BV", "HM", "TF", "BUR", "HVO", "RHO", "DHY", "VDR").contains(countryCode)) {
+
+            try {
+                GeoLocation location = GeoLocation.parse(data[3]);
+                Country.Capital capital = new Country.Capital(data[2], location);
+                capitals.add(capital);
+                Console.println("{0} -> {1}", countryCode, capital);
+
+                if (data.length > 4) {
+                    location = GeoLocation.parse(data[5]);
+                    capital = new Country.Capital(data[4], location);
+                    capitals.add(capital);
+                    Console.println("  {0} -> {1}", countryCode, capital);
+                }
+            } catch (IllegalArgumentException ex) {
+                //ignore
+            }
+
+
+
+
+
+            // String capital = readCapital();
+
+            capitalCities.add(data[2]);
+
+            if (data.length > 3) {
+                capitalCities.add(data[3]);
+            }
         }
-        return capitalCities;
+
+        return capitals;
     }
 
     public String[] readCountryData(String countryCode) {
@@ -75,14 +106,20 @@ public class CountryCapitalReader {
 
     private void readLine(String line) {
         try {
+            List<String> data = new ArrayList<>();
             String[] values = line.split(";");
             String country = values[0];
             String shortName = toShortName(values[1]);
             String longName = values[2].isEmpty() ? shortName : values[2];
-            String capital = values[3];
 
-            String[] data = values.length == 4 ? new String[] {shortName, longName, capital} : new String[] {shortName, longName, capital, values[4]} ;
-            dataByCountry.put(country, data);
+            data.add(shortName);
+            data.add(longName);
+
+            for (int i=3; i<values.length; i++) {
+                data.add(values[i]);
+            }
+
+            dataByCountry.put(country, data.toArray(new String[0]));
         } catch (ArrayIndexOutOfBoundsException ex) {
             throw new RuntimeException("Error on :" + line);
         }
@@ -102,6 +139,7 @@ public class CountryCapitalReader {
 
         return shortName;
     }
+
 
 
 
