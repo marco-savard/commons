9b6d4ed1b2150d9f35f9cb5a21029ea52465c195
Marco Savard <savard.marco@gmail.com>
2025-04-03 14:34:43 -0400

    improved code

--- commons/src/main/java/com/marcosavard/commons/util/collection/NullSafeList.java
+++ commons/src/main/java/com/marcosavard/commons/util/collection/NullSafeList.java
@@ -1,8 +1,16 @@
 package com.marcosavard.commons.util.collection;
 
+import org.apache.poi.xwpf.usermodel.XWPFStyle;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class NullSafeList<T> extends ArrayList<T> {
+
+  public static <T> List<T> of(List<T> originalList) {
+      return (originalList == null) ? new NullSafeList<>() : originalList;
+   }
 
   @Override
   public boolean add(T element) {
@@ -15,6 +23,19 @@ public class NullSafeList<T> extends ArrayList<T> {
 
     if (added) {
       super.add(element);
+    }
+
+    return added;
+  }
+
+
+
+  @Override
+  public boolean addAll(Collection<? extends T> elements) {
+    boolean added = false;
+
+    for (T element : elements) {
+      added = added | add(element);
     }
 
     return added;
