0249dc5aecd5f2891ce03e2c075c3e0bff293ac3
Marco Savard <savard.marco@gmail.com>
2025-04-17 16:45:36 -0400

    improved code

--- commons/src/main/java/com/marcosavard/commons/util/tree/SimpleTreeNode.java
+++
@@ -1,8 +0,0 @@
-package com.marcosavard.commons.util.tree;
-
-public interface SimpleTreeNode<T> extends ITreeNode {
-
-   // public SimpleTreeNode addChild(T childData);
-
-   // void addChildren(T... childData);
-}
