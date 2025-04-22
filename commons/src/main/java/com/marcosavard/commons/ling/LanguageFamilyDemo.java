e02b670e0ce9f233046026d0a252d200961b86a2
Marco Savard <savard.marco@gmail.com>
2025-04-22 14:46:42 -0400

    improved code

--- commons/src/main/java/com/marcosavard/commons/ling/LanguageFamilyDemo.java
+++ commons/src/main/java/com/marcosavard/commons/ling/LanguageFamilyDemo.java
@@ -1,7 +1,7 @@
 package com.marcosavard.commons.ling;
 
 import com.marcosavard.commons.debug.Console;
-import com.marcosavard.commons.io.TreeWriter;
+import com.marcosavard.commons.io.writer.TreeWriter;
 import com.marcosavard.commons.util.collection.ReflectionTreeBuilder;
 
 import javax.swing.tree.TreeNode;
@@ -12,7 +12,6 @@ public class LanguageFamilyDemo {
   public static void main(String[] args) {
     String fieldName = "IndoEuropean";
     TreeNode languageFamily = ReflectionTreeBuilder.of(LanguageFamily.class, fieldName).build();
-    Console.println(languageFamily.toString());
 
     StringWriter sw = new StringWriter();
     TreeWriter tw = new TreeWriter(sw);
