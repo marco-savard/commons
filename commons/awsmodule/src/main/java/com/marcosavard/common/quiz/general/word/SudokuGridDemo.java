d4c1ab81d7374869feb43163345b1535a3bcaffd
Marco Savard <savard.marco@gmail.com>
2025-06-18 11:11:25 -0400

    improved code

---
+++ commons/awsmodule/src/main/java/com/marcosavard/common/quiz/general/word/SudokuGridDemo.java
@@ -0,0 +1,29 @@
+package com.marcosavard.common.quiz.general.word;
+
+import com.marcosavard.common.util.PseudoRandom;
+
+import java.util.Random;
+
+public class SudokuGridDemo {
+
+    public static void main(String[] args) {
+        int seed = 3;
+        Random random = new PseudoRandom(seed);
+        SudokuGrid grid = new SudokuGrid(random);
+        grid.fillGrid();;
+        printGrid(grid); 
+    }
+
+    private static void printGrid(SudokuGrid grid) {
+
+        for (int[] row : grid.getRows()) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+
+            System.out.println();
+        }
+        System.out.println();
+
+    }
+}
