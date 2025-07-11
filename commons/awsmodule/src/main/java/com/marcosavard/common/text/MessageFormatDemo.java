06a9113ca774277ebbd2920c1c6bdabf8c50c934
Marco Savard <savard.marco@gmail.com>
2025-07-11 09:42:01 -0400

    improved class

--- commons/awsmodule/src/main/java/com/marcosavard/common/text/MessageFormatDemo.java
+++ commons/awsmodule/src/main/java/com/marcosavard/common/text/MessageFormatDemo.java
@@ -12,12 +12,15 @@ import java.util.ResourceBundle;
 public class MessageFormatDemo {
     private static final Locale[] DISPLAYS = new Locale[] {Locale.CANADA, Locale.CANADA_FRENCH};
     private static final String MESSAGES = "com.marcosavard.common.text.messages"; //refers to messages.properties
-    private static final String ONLY_PRICE_FOR_DATA_AMOUNT_KEY = "onlyPriceForDataAmount"; //key in messages.properties
-    private static final String ERROR_CHOICE_KEY = "errorChoice"; //key in messages.properties
-    private static final String CABBAGE_CHOICE_KEY = "cabbageChoice"; //key in messages.properties
-    private static final String CHERRY_CHOICE_KEY = "cherryChoice"; //key in messages.properties
-    private static final String LOAF_OF_BREAD_CHOICE_KEY = "loafOfBreadChoice"; //key in messages.properties
-    private static final String GRAPE_LEAF_CHOICE_KEY = "grapeLeafChoice"; //key in messages.properties
+
+    //keys in messages.properties
+    private static final String ONLY_PRICE_FOR_DATA_AMOUNT_MSG_KEY = "only0DollarsFor1DataAmount";
+    private static final String ONLY_PRICE_FOR_DATA_AMOUNT_STR_KEY = "onlyStrDollarsForStrDataAmount";
+    private static final String ERROR_CHOICE_KEY = "errorChoice";
+    private static final String CABBAGE_CHOICE_KEY = "cabbageChoice";
+    private static final String CHERRY_CHOICE_KEY = "cherryChoice";
+    private static final String LOAF_OF_BREAD_CHOICE_KEY = "loafOfBreadChoice";
+    private static final String GRAPE_LEAF_CHOICE_KEY = "grapeLeafChoice";
 
     public static void main(String[] args) {
         printMessageOnlyPriceForDataAmount();
@@ -30,14 +33,19 @@ public class MessageFormatDemo {
         long dataAmount = 20_000_000_000L;
 
         for (int i=0; i<DISPLAYS.length; i++) {
-            Locale display = DISPLAYS[i];
-            printMessage(display, moneyAmount, dataAmount);
+            printMessageUsingStringFormat(DISPLAYS[i], moneyAmount, dataAmount);
         }
+        System.out.println();
+
+        for (int i=0; i<DISPLAYS.length; i++) {
+            printMessageUsingMessageFormat(DISPLAYS[i], moneyAmount, dataAmount);
+        }
+        System.out.println();
     }
 
-    private static void printMessage(Locale display, double moneyAmount, long dataAmount) {
+    private static void printMessageUsingStringFormat(Locale display, double moneyAmount, long dataAmount) {
         ResourceBundle messageBundle = ResourceBundle.getBundle(MESSAGES, display);
-        String pattern = messageBundle.getString(ONLY_PRICE_FOR_DATA_AMOUNT_KEY);
+        String pattern = messageBundle.getString(ONLY_PRICE_FOR_DATA_AMOUNT_STR_KEY);
 
         NumberFormat priceFormat = DecimalFormat.getCurrencyInstance(display);
         NumberFormat dataNumberFormat = DataNumberFormat.getInstance(display);
@@ -45,7 +53,25 @@ public class MessageFormatDemo {
         String price = priceFormat.format(moneyAmount);
         String data = dataNumberFormat.format(dataAmount);
         String lang = display.getDisplayLanguage(display);
-        System.out.println(MessageFormat.format(pattern, lang, price, data));
+
+        if ("fr".equals(display.getLanguage())) {
+            System.out.println(lang + " : " + String.format(pattern, data, price));
+        } else {
+            System.out.println(lang + " : " + String.format(pattern, price, data));
+        }
+    }
+
+    private static void printMessageUsingMessageFormat(Locale display, double moneyAmount, long dataAmount) {
+        ResourceBundle messageBundle = ResourceBundle.getBundle(MESSAGES, display);
+        String pattern = messageBundle.getString(ONLY_PRICE_FOR_DATA_AMOUNT_MSG_KEY);
+
+        NumberFormat priceFormat = DecimalFormat.getCurrencyInstance(display);
+        NumberFormat dataNumberFormat = DataNumberFormat.getInstance(display);
+
+        String price = priceFormat.format(moneyAmount);
+        String data = dataNumberFormat.format(dataAmount);
+        String lang = display.getDisplayLanguage(display);
+        System.out.println(lang + " : " + MessageFormat.format(pattern, price, data));
     }
 
     private static void printMessageErrorWithPlural() {
